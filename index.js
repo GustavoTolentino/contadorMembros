a8d2e5730de91bf0593cab3b9df429a37b6facca
Gustavo Tolentino <tolentinogustavo@gmail.com>
2023-06-08 09:55:55 -0300

    API pronta, subida para teste

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+);
+app.use(express.json());
+app.listen(3000, () => {
+    console.log('Servidor rodando em http://localhost:3000');
+  });
+// app.get('/', (req, res) => {
+//     res.json({ message: 'Api working' });
+// });
+
+let valor = 12010;
+
+// Função para obter a quantidade de dias passados desde uma data específica
+function obterDiasPassados() {
+  const dataEstipulada = new Date('2023-06-07');
+  const dataAtual = new Date();
+  const tempoPassado = dataAtual - dataEstipulada;
+  const diasPassados = Math.floor(tempoPassado / (1000 * 60 * 60 * 24));
+  return diasPassados;
+}
+
+// Função para atualizar o valor a cada 24 horas
+function atualizarValor() {
+  valor += Math.floor(obterDiasPassados() / 1) * 80; // Incremento de 80 a cada 24 horas
+}
+
+app.get('/', (req, res) => {
+  atualizarValor();
+  res.send(`Valor atualizado: ${valor}`);
+});
+
