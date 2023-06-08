7425e27d6c6e09412f8bc54f1d2d8c0f736aa7d2
Gustavo Tolentino <tolentinogustavo@gmail.com>
2023-06-08 10:29:11 -0300

    Teste

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 const express = require('express');
+import { Router } from 'express';
 const app = express();
+const route = Router();
+
 let valor = 12010;
-
-app.use(
-    express.urlencoded({
-        extended: true
-    })
-);
-app.use(express.json());
-app.listen(3000, () => {
-    console.log('Servidor rodando em http://localhost:3000');
-});
-
 
 // Função para obter a quantidade de dias passados desde uma data específica
 function obterDiasPassados() {
-  const dataEstipulada = new Date('2023-06-07');
-  const dataAtual = new Date();
-  const tempoPassado = dataAtual - dataEstipulada;
+    const dataEstipulada = new Date('2023-06-07');
+    const dataAtual = new Date();
+    const tempoPassado = dataAtual - dataEstipulada;
   const diasPassados = Math.floor(tempoPassado / (1000 * 60 * 60 * 24));
   return diasPassados;
 }
 
 // Função para atualizar o valor a cada 24 horas
 function atualizarValor() {
-  valor += Math.floor(obterDiasPassados() / 1) * 80; // Incremento de 80 a cada 24 horas
+    valor += Math.floor(obterDiasPassados() / 1) * 80; // Incremento de 80 a cada 24 horas
 }
 
-app.get('/', (req, res) => {
-  atualizarValor();
-  res.send(`Valor atualizado: ${valor}`);
+route.get('/', (req, res) => {
+    atualizarValor();
+    return res.json({
+        success: true,
+        contador: valor
+    });
 });
-
+app.use(route);
+app.listen(4000, () => {
+    console.log('Servidor rodando em http://localhost:3000');
+});
